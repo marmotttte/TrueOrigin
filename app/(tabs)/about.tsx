fee47e8e60caf4f54844a57a95074e2e691806d5
Manuel Bergeron <manuelbergeron@hotmail.com>
2025-02-15 10:06:25 -0500

    removed the credentials

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -30,8 +30,7 @@ const styles = StyleSheet.create({
   headerImage: {
     color: "#808080",
     bottom: -90,
-    left: -35,
-    position: "absolute",
+    left: -35,    position: "absolute",
   },
   titleContainer: {
     flexDirection: "row",
